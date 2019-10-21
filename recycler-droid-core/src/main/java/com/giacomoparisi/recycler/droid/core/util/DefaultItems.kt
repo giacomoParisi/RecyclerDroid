da065b4fe89a945c4cdcdaacfe7cb91babba54c2
Giacomo Parisi <giacomoparisi95@gmail.com>
2019-10-21 15:24:49 +0200

    Add empty items and viewHolder classes

---
+++ recycler-droid-core/src/main/java/com/giacomoparisi/recycler/droid/core/util/DefaultItems.kt
@@ -0,0 +1,6 @@
+package com.giacomoparisi.recycler.droid.core.util
+
+/**
+ * An empty item for item that not need any data
+ */
+object EmptyItem
---
+++ recycler-droid-core/src/main/java/com/giacomoparisi/recycler/droid/core/util/IDroidItem.kt
@@ -0,0 +1,9 @@
+package com.giacomoparisi.recycler.droid.core.util
+
+/**
+ * Interface that represents the item, useful in the case of lists with items of different types
+ */
+interface IDroidItem {
+
+    val id: String
+}
