002ae0a8fd3dbf6a7654b2a1a9f76f31622506b7
Giacomo Parisi <giacomoparisi95@gmail.com>
2019-11-04 15:34:00 +0100

    Add stable id adapter

---
+++ recycler-droid-core/src/main/java/com/giacomoparisi/recycler/droid/core/adapter/StableDroidAdapter.kt
@@ -0,0 +1,22 @@
+package com.giacomoparisi.recycler.droid.core.adapter
+
+import com.giacomoparisi.recycler.droid.core.DroidAdapter
+import com.giacomoparisi.recycler.droid.core.ViewHolderFactory
+
+class StableDroidAdapter<T : Any>(
+        val getId: (Int) -> Long,
+        defaultFactory: ViewHolderFactory<out T>,
+        areItemsTheSame: (T, T) -> Boolean,
+        areContentsTheSame: (T, T) -> Boolean,
+        getChangePayload: (T, T) -> Any? = { _, _ -> null }
+) : DroidAdapter<T>(defaultFactory, areItemsTheSame, areContentsTheSame, getChangePayload) {
+
+
+    init {
+        this.setHasStableIds(true)
+    }
+
+    override fun getItemId(position: Int): Long {
+        return this.getId(position)
+    }
+}
