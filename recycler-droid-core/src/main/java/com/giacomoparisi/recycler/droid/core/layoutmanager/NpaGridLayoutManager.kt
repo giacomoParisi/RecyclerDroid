c5ba506d540e52385f79def7a635fe1e2087d2e7
Giacomo Parisi <giacomoparisi95@gmail.com>
2019-11-07 18:33:52 +0100

    Add Npa layout managers

---
+++ recycler-droid-core/src/main/java/com/giacomoparisi/recycler/droid/core/layoutmanager/NpaGridLayoutManager.kt
@@ -0,0 +1,25 @@
+package com.giacomoparisi.recycler.droid.core.layoutmanager
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.recyclerview.widget.GridLayoutManager
+
+
+/**
+ * No Predictive Animations GridLayoutManager
+ */
+class NpaGridLayoutManager : GridLayoutManager {
+    /**
+     * Disable predictive animations. There is a bug in RecyclerView which causes views that
+     * are being reloaded to pull invalid ViewHolders from the internal recycler stack if the
+     * adapter size has decreased since the ViewHolder was recycled.
+     */
+    override fun supportsPredictiveItemAnimations(): Boolean = false
+
+
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int, defStyleRes: Int) : super(context, attrs, defStyleAttr, defStyleRes)
+
+    constructor(context: Context, spanCount: Int) : super(context, spanCount)
+
+    constructor(context: Context, spanCount: Int, orientation: Int, reverseLayout: Boolean) : super(context, spanCount, orientation, reverseLayout)
+}
