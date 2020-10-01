e13c45e98db6903a741d95d68bb9859591798e95
Giacomo Parisi <giacomoparisi95@gmail.com>
2020-10-01 11:11:30 +0200

    Update PreCachingLinearLayoutManager to DroidLinearLayoutManager

--- core/src/main/java/com/giacomoparisi/recyclerdroid/core/layoutmanager/DroidLinearLayoutManager.kt
+++ core/src/main/java/com/giacomoparisi/recyclerdroid/core/layoutmanager/DroidLinearLayoutManager.kt
@@ -2,40 +2,33 @@ package com.giacomoparisi.recyclerdroid.core.layoutmanager
 
 import android.content.Context
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 
-open class PreCachingLinearLayoutManager : LinearLayoutManager {
+open class DroidLinearLayoutManager : LinearLayoutManager {
 
-    private val extraLayoutSpace: Int
     private val supportsPredictiveItemAnimations: Boolean
     private val context: Context
 
     constructor(
             context: Context,
-            extraLayoutSpace: Int,
-            supportsPredictiveItemAnimations: Boolean
+            supportsPredictiveItemAnimations: Boolean = true
     ) : super(context) {
         this.context = context
-        this.extraLayoutSpace = extraLayoutSpace
         this.supportsPredictiveItemAnimations = supportsPredictiveItemAnimations
     }
+
     constructor(
             context: Context,
             orientation: Int,
             reverseLayout: Boolean,
-            extraLayoutSpace: Int,
-            supportsPredictiveItemAnimations: Boolean
+            supportsPredictiveItemAnimations: Boolean = true
     ) : super(
             context,
             orientation,
             reverseLayout
     ) {
         this.context = context
-        this.extraLayoutSpace = extraLayoutSpace
         this.supportsPredictiveItemAnimations = supportsPredictiveItemAnimations
     }
-
-    override fun getExtraLayoutSpace(state: RecyclerView.State): Int = extraLayoutSpace
 
     override fun supportsPredictiveItemAnimations(): Boolean = supportsPredictiveItemAnimations
 }
