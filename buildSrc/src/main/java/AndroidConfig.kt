93c529b74d26e1f6235e050e1aee4dacd99bf24c
Giacomo <giacomoparisi95@gmail.com>
2020-11-27 10:18:37 +0100

    Release 1.8.0-beta07

--- buildSrc/src/main/java/AndroidConfig.kt
+++ buildSrc/src/main/java/AndroidConfig.kt
@@ -3,5 +3,5 @@ object AndroidConfig {
     const val target_sdk = 28
     const val compile_sdk = 28
     const val version_code = 1
-    const val version_name = "1.8.0-beta06"
+    const val version_name = "1.8.0-beta07"
 }
